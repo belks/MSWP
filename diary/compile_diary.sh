20af1fa1baebbcce9b202cace973bd3ef3e17692
David Seebacher <dseebacher@gmail.com>
2015-01-08 19:21:01 +0100

    diary submission 4

--- diary/compile_diary.sh
+++ diary/compile_diary.sh
@@ -1,4 +1,4 @@
 #find diary/ -name \*.md -type f -exec pandoc -o {}.tex {} \;
 pandoc -f markdown_phpextra -o diary_bundle.tex reports/*.md
-pdflatex -synctex=1 -interaction=nonstopmode 10_Diary_3.tex
+pdflatex -synctex=1 -interaction=nonstopmode 10_Diary_4.tex
 rm *.out *.log *.synctex.gz *.aux
